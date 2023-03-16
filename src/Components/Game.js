d171ae3a9b4755ad2425e28743fc3c0b66ce4971
charmi-doshi <java.charmi@gmail.com>
2023-03-16 15:39:59 +0530

    moles updated in the code

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -3,35 +3,128 @@ import { render } from "@testing-library/react";
 import React, { Fragment, useState,useEffect,useRef } from "react";
 import Timer from "./Timer";
 
-//asddd
+
 //constants
-const Moles = ({ children }) => <div>{children}</div>;
-const Mole = ({ onWhack }) => {
-  let buttonRef = useRef(null);
+const POINTS_MULTIPLIER = 0.9
+const TIME_MULTIPLIER = 1.25
+
+const generateMoles = amount => new Array(amount).fill().map(() => ({
+  speed: gsap.utils.random(0.5, 1),
+  delay: gsap.utils.random(0.5, 4),
+  points: MOLE_SCORE
+}))
+const Moles = ({ children }) => <div className="moles">{children}</div>
+const Mole = ({ onWhack, points, delay, speed, pointsMin = 10 }) => {
+  const [whacked, setWhacked] = useState(false)
+  const bobRef = useRef(null)
+  const pointsRef = useRef(points)
+  const buttonRef = useRef(null)
   useEffect(() => {
-    gsap.set(buttonRef.current, { yPercent: 100 });
-    gsap.to(buttonRef.current, {
+    gsap.set(buttonRef.current, {
+      yPercent: 100,
+      display: 'block'
+    })
+    bobRef.current = gsap.to(buttonRef.current, {
       yPercent: 0,
+      duration: speed,
       yoyo: true,
       repeat: -1,
-    });
-  }, []);
+      delay: delay,
+      repeatDelay: delay,
+      onRepeat: () => {
+        pointsRef.current = Math.floor(
+          Math.max(pointsRef.current * POINTS_MULTIPLIER, pointsMin)
+        )
+      },
+    })
+    return () => {
+      if (bobRef.current) bobRef.current.kill()
+    }
+  }, [pointsMin, delay, speed])
+  
+  
+  useEffect(() => {
+    if (whacked) {
+      pointsRef.current = points
+      bobRef.current.pause()
+      gsap.to(buttonRef.current, {
+        yPercent: 100,
+        duration: 0.1,
+        onComplete: () => {
+          gsap.delayedCall(gsap.utils.random(1, 3), () => {
+            setWhacked(false)
+            bobRef.current
+             .restart()
+             .timeScale(bobRef.current.timeScale() * TIME_MULTIPLIER)
+          })
+        },
+      })
+    }
+  }, [whacked])
+  
+  const whack = () => {
+    setWhacked(true)
+    onWhack(pointsRef.current)   
+  }
   return (
     <div className="mole-hole">
       <button
         className="mole"
         ref={buttonRef}
-        onClick={() => onWhack(MOLE_SCORE)}
+        onClick={whack}
       >
         Mole
       </button>
     </div>
-  );
-};
+  )
+}
+// const Mole = ({ onWhack, points, delay, speed, pointsMin = 10 }) => {
+//   const bobRef = useRef(null)
+//   const pointsRef = useRef(points)
+//   const buttonRef = useRef(null)
+//   useEffect(() => {
+//     gsap.set(buttonRef.current, {
+//       yPercent: 100,
+//       display: 'block'
+//     })
+//     bobRef.current = gsap.to(buttonRef.current, {
+//       yPercent: 0,
+//       duration: speed,
+//       yoyo: true,
+//       repeat: -1,
+//       delay: delay,
+//       repeatDelay: delay,
+//       onRepeat: () => {
+//         pointsRef.current = Math.floor(
+//           Math.max(pointsRef.current * POINTS_MULTIPLIER, pointsMin)
+//         )
+//       },
+//     })
+//     return () => {
+//       if (bobRef.current) bobRef.current.kill()
+//     }
+//   }, [])
+//   const whack = ()=>{
+//     setWhacked(true)
+//     onWhack(pointsRef.current)
+//   }
+//   return (
+//     <div className="mole-hole">
+//       <button
+//         className="mole"
+//         ref={buttonRef}
+//         onClick={whack}
+//       >
+//         Mole
+//       </button>
+//     </div>
+//   )
+// }
 const Score = ({ value }) => <div>{`Score: ${value}`}</div>;
 
 const TIME_LIMIT = 30000;
 const MOLE_SCORE = 100;
+const NUMBER_OF_MOLES = 6
 
 function Game() {
   //screen and score
@@ -50,7 +143,9 @@ function Game() {
     setFinished(false);
   };
 
-  const onWhack = (points) => setScore(score + points);
+   const onWhack = (points) => setScore(score + points);
+
+
 
   return (
     <Fragment>
@@ -69,12 +164,15 @@ function Game() {
           <Score value={score} />
           <Timer time={TIME_LIMIT} onEnd={endGame} />
           <Moles>
-            <Mole onWhack={onWhack} />
-            <Mole onWhack={onWhack} />
-            <Mole onWhack={onWhack} />
-            <Mole onWhack={onWhack} />
-            <Mole onWhack={onWhack} />
-            <Mole onWhack={onWhack} />
+          {new Array(NUMBER_OF_MOLES).fill().map((_, index) => (
+              <Mole
+                key={index}
+                onWhack={onWhack}
+                points={MOLE_SCORE}
+                delay={0}
+                speed={2}
+              />
+            ))}
           </Moles>
         </Fragment>
       )}
